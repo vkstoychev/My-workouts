8888a261587ab273469ea0484f53542b942e38e7
vkstoychev <vkstoychev@gmail.com>
2024-01-30 20:35:11 +0200

    05 - Final

---
+++ Fundamentals/Arrays - Exercise/05. Top Integers/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace _05._Top_Integers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+                bool isTop = true;
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    int compareNumber = numbers[j];
+
+                    if (compareNumber >= currentNumber)
+                    {
+                        isTop = false;
+                        break;
+                    }
+                }
+
+                if (isTop)
+                {
+                    Console.Write($"{currentNumber} ");
+                }
+            }
+        }
+    }
+}
