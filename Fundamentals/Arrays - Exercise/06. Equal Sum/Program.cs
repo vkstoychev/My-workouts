b49984c2a5b5120f13af48f7976185dbadba4e30
vkstoychev <vkstoychev@gmail.com>
2024-01-30 21:20:39 +0200

    06. - Finish

--- Fundamentals/Arrays - Exercise/06. Equal Sum/Program.cs
+++ Fundamentals/Arrays - Exercise/06. Equal Sum/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
 
+            bool equal = false;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int currentNumber = numbers[i];
+
+                int leftSum = 0;
+                int rightSum = 0;
+
+                for (int j = 0; j < i; j++)
+                {
+                    leftSum += numbers[j];
+                }
+
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    rightSum += numbers[j];
+                }
+
+                if (leftSum == rightSum && !equal)
+                {
+                    Console.WriteLine(i);
+                    equal = true;
+                }
+            }
+            
+            if (!equal)
+            {
+                Console.WriteLine("no");
+            }
         }
     }
 }
