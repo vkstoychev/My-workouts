de02b5e046712e7db101aadbaab3f4f86a6fd5b1
vkstoychev <vkstoychev@gmail.com>
2024-01-30 21:22:03 +0200

    07. - Begin

---
+++ Fundamentals/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _07._Max_Sequence_of_Equal_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
