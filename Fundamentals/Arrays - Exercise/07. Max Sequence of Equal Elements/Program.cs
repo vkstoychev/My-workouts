9ddbfb60529073c4fa0a4f2f00ffa6fd1ef4d54b
vkstoychev <vkstoychev@gmail.com>
2024-01-30 22:05:52 +0200

    07. Finish

--- Fundamentals/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
+++ Fundamentals/Arrays - Exercise/07. Max Sequence of Equal Elements/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] symbols = Console.ReadLine().Split();
+
+            int bestCount = 0;
+            string bestCountSymbol = "";
+
+            for (int i = symbols.Length - 1; i >= 0; i--)
+            {
+                int count = 1;
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    if (symbols[i] == symbols[j])
+                    {
+                        count++;
+
+                        if (bestCount <= count)
+                        {
+                            bestCount = count;
+                            bestCountSymbol = symbols[i];
+                        }
+                    }
+
+                    else
+                    {
+                        break;
+                    }
+                }
+            }
+
+            string[] result = new string[bestCount];
+            Array.Fill(result, bestCountSymbol);
+            Console.WriteLine(string.Join(' ', result));
+
+            //for (int i = 0; i < bestCount; i++)
+            //{
+            //    Console.Write($"{bestCountSymbol} ");
+            //}
+
         }
     }
 }
